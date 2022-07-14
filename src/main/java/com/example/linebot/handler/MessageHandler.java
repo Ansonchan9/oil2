fe09bfc82efab02932bf80cd522793d31a9846c5
Ansonchan9 <108714794+Ansonchan9@users.noreply.github.com>
2022-07-14 10:19:11 +0800

    Add files via upload

--- src/main/java/com/example/linebot/handler/MessageHandler.java
+++ src/main/java/com/example/linebot/handler/MessageHandler.java
@@ -43,55 +43,122 @@ public class MessageHandler {
 		Element items = document.getElementById("fixlayer");
 		Document document1 = Jsoup.connect("https://music-tw.line.me/chart/99").get();
 		Element items1 = document1.getElementById("content");
+//嘉義市
+		Document document20 = Jsoup.connect("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-C61A529F-C4D3-499A-8DFC-D1C585AED84A&format=XML&locationName=%E5%98%89%E7%BE%A9%E5%B8%82").get();
+		Elements items20 = document20.getElementsByTag("dataset");
+		for (Element item20 : items20) {
+			String list = item20.getElementsByTag("datasetDescription").get(0).text();
+			String loca201 = item20.getElementsByTag("locationName").get(0).text();
+			String st201 = item20.getElementsByTag("startTime").get(0).text();
+			String et201 = item20.getElementsByTag("endTime").get(0).text();
+			String st202 = item20.getElementsByTag("startTime").get(1).text();
+			String et202 = item20.getElementsByTag("endTime").get(1).text();
+			String st203 = item20.getElementsByTag("startTime").get(2).text();
+			String et203 = item20.getElementsByTag("endTime").get(2).text();
+			String wx201 = item20.getElementsByTag("parameterName").get(0).text();
+			String wx202 = item20.getElementsByTag("parameterName").get(1).text();
+			String wx203 = item20.getElementsByTag("parameterName").get(2).text();
+			String pop201 = item20.getElementsByTag("parameterName").get(3).text();
+			String pop202 = item20.getElementsByTag("parameterName").get(4).text();
+			String pop203 = item20.getElementsByTag("parameterName").get(5).text();
+			String mt201 = item20.getElementsByTag("parameterName").get(6).text();
+			String mt202 = item20.getElementsByTag("parameterName").get(7).text();
+			String mt203 = item20.getElementsByTag("parameterName").get(8).text();
+			String ht201 = item20.getElementsByTag("parameterName").get(12).text();
+			String ht202 = item20.getElementsByTag("parameterName").get(13).text();
+			String ht203 = item20.getElementsByTag("parameterName").get(14).text();
+			String ci201 = item20.getElementsByTag("parameterName").get(9).text();
+			String ci202 = item20.getElementsByTag("parameterName").get(10).text();
+			String ci203 = item20.getElementsByTag("parameterName").get(11).text();
 
-		String oilup = items.getElementsByClass("update").get(0).text();
-		String oil = items.getElementsByClass("main").get(0).text();String rank=items1.getElementsByClass("text").get(0).text();
-			String list=items1.getElementsByClass("end_title").get(0).text();
-			String song1=items1.getElementsByClass("link_text").get(0).text();
-			String singer1=items1.getElementsByClass("link_artist").get(0).text();
-			String rank2=items1.getElementsByClass("text").get(1).text();
-			String song2=items1.getElementsByClass("link_text").get(1).text();
-			String singer2=items1.getElementsByClass("link_artist").get(2).text();
-			String rank3=items1.getElementsByClass("text").get(2).text();
-			String song3=items1.getElementsByClass("link_text").get(2).text();
-			String singer3=items1.getElementsByClass("link_artist").get(4).text();
-			String rank4=items1.getElementsByClass("text").get(3).text();
-			String song4=items1.getElementsByClass("link_text").get(3).text();
-			String singer4=items1.getElementsByClass("link_artist").get(6).text();
-			String rank5=items1.getElementsByClass("text").get(4).text();
-			String song5=items1.getElementsByClass("link_text").get(4).text();
-			String singer5=items1.getElementsByClass("link_artist").get(8).text();
 
-		JSONObject body = new JSONObject();
-		JSONArray messages = new JSONArray();
-		JSONObject message = new JSONObject();
-		message.put("type", "text");
-		switch (text){
-			case "你好":
-				message.put("text", "哈囉，我是全能小幫手Bnson");
-				break;
-			case "下周油價":
-				message.put("text", "下周油價："+"\n"+oil+"\n"+oilup);
-				break;
-			case "下週油價":
-				message.put("text", "下週油價："+"\n"+oil+"\n"+oilup);
-				break;
-			case "華語歌曲推薦":
-				message.put("text", list+"\n"+rank+"\n"+"歌名："+song1+"\n"+"演唱人："+singer1+"\n"
-						+rank2+"\n"+"歌名："+song2+"\n"+"演唱人："+singer2+"\n"
-						+rank3+"\n"+"歌名："+song3+"\n"+"演唱人："+singer3+"\n"
-						+rank4+"\n"+"歌名："+song4+"\n"+"演唱人："+singer4+"\n"
-						+rank5+"\n"+"歌名："+song5+"\n"+"演唱人："+singer5);
-				break;
-			default:
-				message.put("text", "我還看不懂，我目前只看得懂以下指令"+"\n"+":下周油價"+"\n"+":音樂推薦");
-			break;
-		}
-		messages.put(message);
-		body.put("replyToken", replyToken);
-		body.put("messages", messages);
-		sendLinePlatform(body);
+		//屏東縣
+		Document document21 = Jsoup.connect("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-C61A529F-C4D3-499A-8DFC-D1C585AED84A&format=XML&locationName=%E5%B1%8F%E6%9D%B1%E7%B8%A3").get();
+		Elements items21 = document21.getElementsByTag("dataset");
+		for (Element item21 : items21) {
+			String loca211 = item21.getElementsByTag("locationName").get(0).text();
+			String st211 = item21.getElementsByTag("startTime").get(0).text();
+			String et211 = item21.getElementsByTag("endTime").get(0).text();
+			String st212 = item21.getElementsByTag("startTime").get(1).text();
+			String et212 = item21.getElementsByTag("endTime").get(1).text();
+			String st213 = item21.getElementsByTag("startTime").get(2).text();
+			String et213 = item21.getElementsByTag("endTime").get(2).text();
+			String wx211 = item21.getElementsByTag("parameterName").get(0).text();
+			String wx212 = item21.getElementsByTag("parameterName").get(1).text();
+			String wx213 = item21.getElementsByTag("parameterName").get(2).text();
+			String pop211 = item21.getElementsByTag("parameterName").get(3).text();
+			String pop212 = item21.getElementsByTag("parameterName").get(4).text();
+			String pop213 = item21.getElementsByTag("parameterName").get(5).text();
+			String mt211 = item21.getElementsByTag("parameterName").get(6).text();
+			String mt212 = item21.getElementsByTag("parameterName").get(7).text();
+			String mt213 = item21.getElementsByTag("parameterName").get(8).text();
+			String ht211 = item21.getElementsByTag("parameterName").get(12).text();
+			String ht212 = item21.getElementsByTag("parameterName").get(13).text();
+			String ht213 = item21.getElementsByTag("parameterName").get(14).text();
+			String ci211 = item21.getElementsByTag("parameterName").get(9).text();
+			String ci212 = item21.getElementsByTag("parameterName").get(10).text();
+			String ci213 = item21.getElementsByTag("parameterName").get(11).text();
 
+
+			String oilup = items.getElementsByClass("update").get(0).text();
+			String oil = items.getElementsByClass("main").get(0).text();
+			String rank = items1.getElementsByClass("text").get(0).text();
+			String list1 = items1.getElementsByClass("end_title").get(0).text();
+			String song1 = items1.getElementsByClass("link_text").get(0).text();
+			String singer1 = items1.getElementsByClass("link_artist").get(0).text();
+			String rank2 = items1.getElementsByClass("text").get(1).text();
+			String song2 = items1.getElementsByClass("link_text").get(1).text();
+			String singer2 = items1.getElementsByClass("link_artist").get(2).text();
+			String rank3 = items1.getElementsByClass("text").get(2).text();
+			String song3 = items1.getElementsByClass("link_text").get(2).text();
+			String singer3 = items1.getElementsByClass("link_artist").get(4).text();
+			String rank4 = items1.getElementsByClass("text").get(3).text();
+			String song4 = items1.getElementsByClass("link_text").get(3).text();
+			String singer4 = items1.getElementsByClass("link_artist").get(6).text();
+			String rank5 = items1.getElementsByClass("text").get(4).text();
+			String song5 = items1.getElementsByClass("link_text").get(4).text();
+			String singer5 = items1.getElementsByClass("link_artist").get(8).text();
+
+			JSONObject body = new JSONObject();
+			JSONArray messages = new JSONArray();
+			JSONObject message = new JSONObject();
+			message.put("type", "text");
+			switch (text) {
+				case "你好":
+					message.put("text", "哈囉，我是全能小幫手Bnson");
+					break;
+				case "下周油價":
+					message.put("text", "下周油價：" + "\n" + oil + "\n" + oilup);
+					break;
+				case "下週油價":
+					message.put("text", "下週油價：" + "\n" + oil + "\n" + oilup);
+					break;
+				case "華語歌曲推薦":
+					message.put("text", list1 + "\n" + rank + "\n" + "歌名：" + song1 + "\n" + "演唱人：" + singer1 + "\n"
+							+ rank2 + "\n" + "歌名：" + song2 + "\n" + "演唱人：" + singer2 + "\n"
+							+ rank3 + "\n" + "歌名：" + song3 + "\n" + "演唱人：" + singer3 + "\n"
+							+ rank4 + "\n" + "歌名：" + song4 + "\n" + "演唱人：" + singer4 + "\n"
+							+ rank5 + "\n" + "歌名：" + song5 + "\n" + "演唱人：" + singer5);
+					break;
+				case "天氣 屏東縣":
+					message.put("text", list + "\n" + loca211 + "\n" + st211 + "～" + et211 + "\n" + "氣溫：" + mt211 + "度" + "~" + ht211 + "度" + "\n" + "氣候：" + wx211 + "\n" + "降雨機率：" + pop211 + "%" + "\n" + "舒適度：" + ci211 + "\n"
+							+ st212 + "～" + et212 + "\n" + "氣溫：" + mt212 + "度" + "~" + ht212 + "度" + "\n" + "氣候：" + wx212 + "\n" + "降雨機率：" + pop212 + "%" + "\n" + "舒適度：" + ci212 + "\n"
+							+ st213 + "～" + et213 + "\n" + "氣溫：" + mt213 + "度" + "~" + ht213 + "度" + "\n" + "氣候：" + wx213 + "\n" + "降雨機率：" + pop213 + "%" + "\n" + "舒適度：" + ci213 + "\n");
+					break;
+				case "天氣 嘉義市":
+					message.put("text", list + "\n" + loca201 + "\n" + st201 + "～" + et201 + "\n" + "氣溫：" + mt201 + "度" + "~" + ht201 + "度" + "\n" + "氣候：" + wx201 + "\n" + "降雨機率：" + pop201 + "%" + "\n" + "舒適度：" + ci201 + "\n"
+							+ st202 + "～" + et202 + "\n" + "氣溫：" + mt202 + "度" + "~" + ht202 + "度" + "\n" + "氣候：" + wx202 + "\n" + "降雨機率：" + pop202 + "%" + "\n" + "舒適度：" + ci202 + "\n"
+							+ st203 + "～" + et203 + "\n" + "氣溫：" + mt203 + "度" + "~" + ht203 + "度" + "\n" + "氣候：" + wx203 + "\n" + "降雨機率：" + pop203 + "%" + "\n" + "舒適度：" + ci203 + "\n");
+					break;
+				default:
+					message.put("text", "我還看不懂，我目前只看得懂以下指令" + "\n" + ":下周油價" + "\n" + ":音樂推薦");
+					break;
+			}
+			messages.put(message);
+			body.put("replyToken", replyToken);
+			body.put("messages", messages);
+			sendLinePlatform(body);
+		}}
 		}
 
 
