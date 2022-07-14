31f725cdc264cd5eec18d66683a206a5388e68aa
Ansonchan9 <108714794+Ansonchan9@users.noreply.github.com>
2022-07-14 10:31:43 +0800

    Add files via upload

--- src/main/java/com/example/linebot/handler/MessageHandler.java
+++ src/main/java/com/example/linebot/handler/MessageHandler.java
@@ -43,11 +43,73 @@ public class MessageHandler {
 		Element items = document.getElementById("fixlayer");
 		Document document1 = Jsoup.connect("https://music-tw.line.me/chart/99").get();
 		Element items1 = document1.getElementById("content");
+		//南投縣
+		Document document17 = Jsoup.connect("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-C61A529F-C4D3-499A-8DFC-D1C585AED84A&format=XML&locationName=%E5%8D%97%E6%8A%95%E7%B8%A3").get();
+		Elements items17 = document17.getElementsByTag("dataset");
+		for (Element item : items17) {
+			String list = item.getElementsByTag("datasetDescription").get(0).text();
+			String loca1 = item.getElementsByTag("locationName").get(0).text();
+			String st1 = item.getElementsByTag("startTime").get(0).text();
+			String et1 = item.getElementsByTag("endTime").get(0).text();
+			String st2 = item.getElementsByTag("startTime").get(1).text();
+			String et2 = item.getElementsByTag("endTime").get(1).text();
+			String st3 = item.getElementsByTag("startTime").get(2).text();
+			String et3 = item.getElementsByTag("endTime").get(2).text();
+			String wx1 = item.getElementsByTag("parameterName").get(0).text();
+			String wx2 = item.getElementsByTag("parameterName").get(1).text();
+			String wx3 = item.getElementsByTag("parameterName").get(2).text();
+			String pop1 = item.getElementsByTag("parameterName").get(3).text();
+			String pop2 = item.getElementsByTag("parameterName").get(4).text();
+			String pop3 = item.getElementsByTag("parameterName").get(5).text();
+			String mt1 = item.getElementsByTag("parameterName").get(6).text();
+			String mt2 = item.getElementsByTag("parameterName").get(7).text();
+			String mt3 = item.getElementsByTag("parameterName").get(8).text();
+			String ht1 = item.getElementsByTag("parameterName").get(12).text();
+			String ht2 = item.getElementsByTag("parameterName").get(13).text();
+			String ht3 = item.getElementsByTag("parameterName").get(14).text();
+			String ci1 = item.getElementsByTag("parameterName").get(9).text();
+			String ci2 = item.getElementsByTag("parameterName").get(10).text();
+			String ci3 = item.getElementsByTag("parameterName").get(11).text();
+
+			JSONObject message = new JSONObject();
+			message.put("type", "天氣 南投縣");
+			message.put("text",list + "\n" + loca1 + "\n" + st1 + "～" + et1 + "\n" + "氣溫：" + mt1 + "度" + "~" + ht1 + "度" + "\n"+ "氣候：" + wx1 + "\n" + "降雨機率：" + pop1 + "%" + "\n"  + "舒適度：" + ci1 + "\n"
+					+ st2 + "～" + et2 + "\n"  + "氣溫：" + mt2 + "度" + "~" + ht2 + "度" + "\n"+ "氣候：" + wx2 + "\n" + "降雨機率：" + pop2 + "%" + "\n" + "舒適度：" + ci2 + "\n"
+					+ st3 + "～" + et3 + "\n" + "氣溫：" + mt3 + "度" + "~" + ht3 + "度" + "\n"+ "氣候：" + wx3 + "\n" + "降雨機率：" + pop3 + "%" + "\n"  + "舒適度：" + ci3 + "\n");
+
+		}
+//雲林縣
+		Document document18 = Jsoup.connect("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-C61A529F-C4D3-499A-8DFC-D1C585AED84A&format=XML&locationName=%E9%9B%B2%E6%9E%97%E7%B8%A3").get();
+		Elements items18 = document18.getElementsByTag("dataset");
+		for (Element item18 : items18) {
+			String list = item18.getElementsByTag("datasetDescription").get(0).text();
+			String loca181 = item18.getElementsByTag("locationName").get(0).text();
+			String st181 = item18.getElementsByTag("startTime").get(0).text();
+			String et181 = item18.getElementsByTag("endTime").get(0).text();
+			String st182 = item18.getElementsByTag("startTime").get(1).text();
+			String et182 = item18.getElementsByTag("endTime").get(1).text();
+			String st183 = item18.getElementsByTag("startTime").get(2).text();
+			String et183 = item18.getElementsByTag("endTime").get(2).text();
+			String wx181 = item18.getElementsByTag("parameterName").get(0).text();
+			String wx182 = item18.getElementsByTag("parameterName").get(1).text();
+			String wx183 = item18.getElementsByTag("parameterName").get(2).text();
+			String pop181 = item18.getElementsByTag("parameterName").get(3).text();
+			String pop182 = item18.getElementsByTag("parameterName").get(4).text();
+			String pop183 = item18.getElementsByTag("parameterName").get(5).text();
+			String mt181 = item18.getElementsByTag("parameterName").get(6).text();
+			String mt182 = item18.getElementsByTag("parameterName").get(7).text();
+			String mt183 = item18.getElementsByTag("parameterName").get(8).text();
+			String ht181 = item18.getElementsByTag("parameterName").get(12).text();
+			String ht182 = item18.getElementsByTag("parameterName").get(13).text();
+			String ht183 = item18.getElementsByTag("parameterName").get(14).text();
+			String ci181 = item18.getElementsByTag("parameterName").get(9).text();
+			String ci182 = item18.getElementsByTag("parameterName").get(10).text();
+			String ci183 = item18.getElementsByTag("parameterName").get(11).text();
+
 //嘉義市
 		Document document20 = Jsoup.connect("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-C61A529F-C4D3-499A-8DFC-D1C585AED84A&format=XML&locationName=%E5%98%89%E7%BE%A9%E5%B8%82").get();
 		Elements items20 = document20.getElementsByTag("dataset");
 		for (Element item20 : items20) {
-			String list = item20.getElementsByTag("datasetDescription").get(0).text();
 			String loca201 = item20.getElementsByTag("locationName").get(0).text();
 			String st201 = item20.getElementsByTag("startTime").get(0).text();
 			String et201 = item20.getElementsByTag("endTime").get(0).text();
@@ -70,8 +132,6 @@ public class MessageHandler {
 			String ci201 = item20.getElementsByTag("parameterName").get(9).text();
 			String ci202 = item20.getElementsByTag("parameterName").get(10).text();
 			String ci203 = item20.getElementsByTag("parameterName").get(11).text();
-
-
 		//屏東縣
 		Document document21 = Jsoup.connect("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-C61A529F-C4D3-499A-8DFC-D1C585AED84A&format=XML&locationName=%E5%B1%8F%E6%9D%B1%E7%B8%A3").get();
 		Elements items21 = document21.getElementsByTag("dataset");
@@ -150,6 +210,11 @@ public class MessageHandler {
 							+ st202 + "～" + et202 + "\n" + "氣溫：" + mt202 + "度" + "~" + ht202 + "度" + "\n" + "氣候：" + wx202 + "\n" + "降雨機率：" + pop202 + "%" + "\n" + "舒適度：" + ci202 + "\n"
 							+ st203 + "～" + et203 + "\n" + "氣溫：" + mt203 + "度" + "~" + ht203 + "度" + "\n" + "氣候：" + wx203 + "\n" + "降雨機率：" + pop203 + "%" + "\n" + "舒適度：" + ci203 + "\n");
 					break;
+				case "天氣 雲林縣":
+					message.put("text", list + "\n" + loca181 + "\n" + st181 + "～" + et181 + "\n" + "氣溫：" + mt181 + "度" + "~" + ht181 + "度" + "\n" + "氣候：" + wx181 + "\n" + "降雨機率：" + pop181 + "%" + "\n" + "舒適度：" + ci181 + "\n"
+							+ st182 + "～" + et182 + "\n" + "氣溫：" + mt182 + "度" + "~" + ht182 + "度" + "\n" + "氣候：" + wx182 + "\n" + "降雨機率：" + pop182 + "%" + "\n" + "舒適度：" + ci182 + "\n"
+							+ st183 + "～" + et183 + "\n" + "氣溫：" + mt183 + "度" + "~" + ht183 + "度" + "\n" + "氣候：" + wx183 + "\n" + "降雨機率：" + pop183 + "%" + "\n" + "舒適度：" + ci183 + "\n");
+					break;
 				default:
 					message.put("text", "我還看不懂，我目前只看得懂以下指令" + "\n" + ":下周油價" + "\n" + ":音樂推薦");
 					break;
@@ -158,7 +223,7 @@ public class MessageHandler {
 			body.put("replyToken", replyToken);
 			body.put("messages", messages);
 			sendLinePlatform(body);
-		}}
+		}}}
 		}
 
 
