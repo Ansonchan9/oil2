c44695b62258468ba089527cbcdddf4844948373
Ansonchan9 <108714794+Ansonchan9@users.noreply.github.com>
2024-03-31 19:46:49 +0800

    Update MessageHandler.java

--- src/main/java/com/example/linebot/handler/MessageHandler.java
+++ src/main/java/com/example/linebot/handler/MessageHandler.java
@@ -7,6 +7,7 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
